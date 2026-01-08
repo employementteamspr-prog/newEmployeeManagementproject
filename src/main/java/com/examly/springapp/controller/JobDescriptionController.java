57e69d1aa525944300b30fbabac872648753d16f
SanthaaKumar-p <ss4921203@gmail.com>
2026-01-08 12:28:14 +0530

    Initial Spring Boot backend commit

---
+++ src/main/java/com/examly/springapp/controller/JobDescriptionController.java
@@ -0,0 +1,8 @@
+package com.examly.springapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class JobDescriptionController {
+    
+}
---
+++ src/main/java/com/examly/springapp/repository/UserRepo.java
@@ -0,0 +1,13 @@
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.Employee;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+
+public interface UserRepo extends JpaRepository<Employee, Long> {
+
+}
