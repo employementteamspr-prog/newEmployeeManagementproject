dc01b23ab47628fd2b186c9f259a6d723aaf1f9e
Prithik-Aswin-S <727724euit181@skcet.ac.in>
2026-01-09 13:15:29 +0530

    added by prithik

---
+++ Employeemanagement-main/springapp/src/main/java/com/examly/springapp/repository/JobDescriptionRepo.java
@@ -0,0 +1,12 @@
+package com.examly.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.examly.springapp.model.JobDescription; 
+
+
+
+public interface JobDescriptionRepo extends JpaRepository<JobDescription, Integer> {
+
+    
+}
